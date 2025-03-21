48ce333e5a22e2ceaf1180b69a42929f3d99d369
akillez01 <achilles.oliveira.souza@gmail.com>
2025-03-21 18:29:45 -0500

    feat(baclend):schema do banco concluido

---
+++ backend/prisma/migrations/20250321230847_fix_dropped_tables/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - You are about to drop the `_ProjetoToTecnologia` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "_ProjetoToTecnologia" DROP CONSTRAINT "_ProjetoToTecnologia_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_ProjetoToTecnologia" DROP CONSTRAINT "_ProjetoToTecnologia_B_fkey";
+
+-- DropTable
+DROP TABLE "_ProjetoToTecnologia";
